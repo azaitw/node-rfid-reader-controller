bc8236790bc0efdaea6a24ca91bcf5c471e105e0
Kai-Sheng Chan <azai@Azai-MBP.local>
2017-05-10 23:11:25 +0800

    Typo

--- env-config/development.js
+++ env-config/development.js
@@ -5,6 +5,6 @@ module.exports = {
         reader: '192.168.0.100'
     },
     //fileLoggerPath: '/Users/azai/webserver/race/readerLogs/',
-    fileLoggerPath: '/home/pi/webserver/logs',
+    fileLoggerPath: '/home/pi/webserver/logs/',
     port: 1337
 };
